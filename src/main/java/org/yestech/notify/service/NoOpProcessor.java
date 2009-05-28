9822af0a76abca37bc4f87ded3247d683f2b26b9
Artie Copeland <artie@yestech.org>
2009-05-27 23:45:57 -0700

    added a no op processor

---
+++ src/main/java/org/yestech/notify/service/NoOpProcessor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
+
+/*
+ *
+ * Author:  Artie Copeland
+ * Last Modified Date: $DateTime: $
+ */
+package org.yestech.notify.service;
+
+import org.yestech.notify.objectmodel.INotificationJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Does a NoOp.  Ususally used for testing.
+ * 
+ * @author Artie Copeland
+ * @version $Revision: $
+ */
+public class NoOpProcessor implements INotificationProcessor {
+    final private static Logger logger = LoggerFactory.getLogger(NoOpProcessor.class);
+
+    @Override
+    public void process(INotificationJob notificationJob) {
+        if (logger.isInfoEnabled()) {
+            logger.info("Called with job: " + notificationJob);
+        }
+    }
+}
